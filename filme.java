485069edbcd07d84e0c1625ed337fdb997613072
brunowigers <bruno.wigers@escola.pr.gov.br>
2026-06-17 13:02:14 +0000

    ok1

---
+++ filme.java
@@ -0,0 +1,22 @@
+public class filme {
+    public static void main(String[] args) {
+        System.out.println("Bem vindos ao TecFlix");
+        System.out.println("filme: O Homem de Toronto");
+
+        int anoDeLancamento = 1987;
+        System.out.println("Ano de lançamento" + anoDeLancamento);
+        booelean incluidoNoPlano = true;
+        double notaDoFilme = 5.9;
+        double media = (6.6 + 9.5 + 10)/ 3;
+        System.out.println("A media que minha turma deu" + media);
+
+        String sinopse;
+
+        sinopse = """
+        O filme do Homen de toronto
+        Estrelado por Kevin Hart e Woody Harrelson
+        o filme tem nota mediana
+        e foi lançado em
+        """ + ano
+    }
+}
